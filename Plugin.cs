598f4ad256cfa641c9582726fd22cac020ee849d
AWildErin <sharpian126@gmail.com>
2023-07-22 01:41:25 +0100

    Initial plugin

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace SODPresence;
+
+[BepInPlugin( MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION )]
+public class Plugin : BasePlugin
+{
+	public static ManualLogSource Logger;
+
+	public override void Load()
+	{
+		Logger = Log;
+
+		// Plugin startup logic
+		Log.LogInfo( $"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!" );
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Shadows of Doubt: Presence
+A mod that adds Discord rich presence to Shadows of Doubt
